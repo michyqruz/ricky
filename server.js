7b6931aa9dc922b09fbdc51ada2473b53cdfccb8
Moneyboy.buy <michyqruz@gmail.com>
2025-08-15 09:34:11 +0100

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
     user: 'paypal.services.ion@gmail.com', // Replace with your Gmail address
-    pass: 'xkzvckanmienbavg'      // Replace with your Gmail App Password
+    pass: 'xkzvckanmienba'      // Replace with your Gmail App Password
   }
 });
 
