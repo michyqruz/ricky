8dde38964792292534d04dac5332a7c374c36725
Moneyboy.buy <michyqruz@gmail.com>
2025-08-22 18:35:44 +0100

    Update server.js

--- server.js
+++ server.js
@@ -47,8 +47,8 @@ const transporter = nodemailer.createTransport({
     text: `Welcome to Our Newsletter. ${sanitizedName}!\n\nThank you for subscribing! This email contains updates and images in HTML format, but your email client is displaying the plain-text version. Visit our website for more information: https://example.com`,
     html: `
   <p style="text-align: center; color: #999999; font-size: 12px;">Hello ${sanitizedName}</p>
-  <div style="font-family: Helvetica, Arial, sans-serif; max-width: 600px; margin: 0 auto; text-align: center; padding: 20px; background-color: #f5f5f5;">
-    <img src="https://freshknots.in/wp-content/uploads/2023/03/rose.png" alt="logo" style="width: 50px; height: 50px; border-radius: 50%; object-fit: cover; border: 2px solid #3498db; box-shadow: 0 4px 10px rgba(0, 0, 0, 0.1);">
+  <div style="font-family: Helvetica, Arial, sans-serif; max-width: 600px; margin: 0 auto; text-align: center; padding: 20px; background-color: #f1f4f7;">
+    <img src="https://michyqruz.github.io/The-money-box/IMG_8211.JPG" alt="logo" style="width: 60px; height: 60px; border-radius: 50%; object-fit: cover;">
     <h1 style="color: #000000; font-size: 36px; margin: 40px 0; font-weight: 900;">Welcome to Our Newsletter</h1>
     <p style="color: #000000; font-size: 20px; font-weight: bold; text-align: left; line-height: 1.5;">
         Thank you for subscribing! Here's a test email with some thumbnail images. Here's a test email with some thumbnail images.
@@ -56,9 +56,9 @@ const transporter = nodemailer.createTransport({
     <a href="https://millxore.github.io/start/" style="display: inline-block; color: #ffffff; padding: 15px 40px; background-color: #000000; color: white; text-decoration: none; border-radius: 25px; font-size: 16px; margin: 10px 0; font-weight: bold;">Explore</a>
   </div>
       
-      <div style="background-color: #f5f5f5; max-width: 600px; margin: 0 auto; padding: 20px;">
+      <div style="background-color: #f1f4f7; max-width: 600px; margin: 0 auto; padding: 20px;">
           <div style="text-align: center;">
-              <img src="https://freshknots.in/wp-content/uploads/2023/03/rose.png" alt="logo" style="width: 50px; height: 50px; border-radius: 50%; object-fit: cover; border: 2px solid #3498db; box-shadow: 0 4px 10px rgba(0, 0, 0, 0.1);">
+              <img src="https://michyqruz.github.io/The-money-box/IMG_8211.JPG" alt="logo" style="width: 60px; height: 60px; border-radius: 50%; object-fit: cover;">
               <hr style="margin: 30px auto; border: 1px solid #D3D3D3;">
           </div>
           <div style="text-align: center;">
@@ -67,22 +67,19 @@ const transporter = nodemailer.createTransport({
               <a href="#" style="font-size: 12px; color: #1a73e8; text-decoration: none; padding: 0 8px; border-left: 2px solid black;">Apps</a>
           </div>
           <p style="color: #555; font-size: 14px;">
-      For more new updates, visit our website! For more new updates, visit our website! For more new updates, visit our website!
+     We are committed to preventing fraudulent emails. Emails from us will always contain your full name. <a href="#" style="font-size: 12px; color: #1a73e8; text-decoration: none;">Learn to identify phishing</a>
           </p>
           <p style="color: #555; font-size: 14px;">
-      For more new updates, visit our website!   
+      Please don't reply to this email. To get in touch with us, click <a href="#" style="font-size: 12px; color: #1a73e8; text-decoration: none;">Help & Contact.</a>   
           </p>
           <p style="color: #555; font-size: 14px;">
-      For more new updates, visit our website! For more new updates, visit our website!
+      Not sure why you received this email? <a href="#" style="font-size: 12px; color: #1a73e8; text-decoration: none;">Learn more</a>
           </p>
           <p style="color: #555; font-size: 14px;">
-      For more new updates, visit our website!   
+      Copyright © 1999-2025. All rights reserved. 
           </p>
           <p style="color: #555; font-size: 14px;">
-      For more new updates, visit our website!   
-          </p>
-          <p style="color: #555; font-size: 14px;">
-      For more new updates, visit our website! For more new updates, visit our website! For more new updates, visit our website!
+      Rts. Ltd. is licensed by the Monetary Authority of Singapore as a Major Payment Institution under the Payment Services Act 2019.  
           </p>
       </div>
 `
