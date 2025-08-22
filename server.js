2ff5a52f4f32042256525d8e9dcfbd680a3cf59f
Moneyboy.buy <michyqruz@gmail.com>
2025-08-22 16:36:59 +0100

    Update server.js

--- server.js
+++ server.js
@@ -46,10 +46,10 @@ const transporter = nodemailer.createTransport({
     subject: 'A transaction is pending and been reviewed (Ref ID - TY-U-753194873157)',
     text: `Welcome to Our Newsletter. ${sanitizedName}!\n\nThank you for subscribing! This email contains updates and images in HTML format, but your email client is displaying the plain-text version. Visit our website for more information: https://example.com`,
     html: `
-  <p style="text-align: center;">Hello ${sanitizedName}</p>
+  <<p style="text-align: center; color: #999999;">Hello ${sanitizedName}</p>
   <div style="font-family: Helvetica, Arial, sans-serif; font-weight: bold; max-width: 600px; margin: 0 auto; text-align: center; padding: 20px; background-color: #f5f5f5;">
     <img src="https://freshknots.in/wp-content/uploads/2023/03/rose.png" alt="logo" style="width: 50px; height: 50px; border-radius: 50%; object-fit: cover; border: 2px solid #3498db; box-shadow: 0 4px 10px rgba(0, 0, 0, 0.1);">
-    <h1 style="font-size: 36px; margin: 40px 0;">Welcome to Our Newsletter</h1>
+    <h1 style="font-size: 36px; margin: 40px 0; font-weight: 900;">Welcome to Our Newsletter</h1>
     <p style="font-size: 20px; text-align: left; line-height: 1.5;">
         Thank you for subscribing! Here's a test email with some thumbnail images. Here's a test email with some thumbnail images.
     </p>
@@ -66,11 +66,11 @@ const transporter = nodemailer.createTransport({
       <a href="#" style="font-size: 12px; color: #1a73e8; text-decoration: none; padding: 0 8px; border-left: 2px solid black;">Security</a>
       <a href="#" style="font-size: 12px; color: #1a73e8; text-decoration: none; padding: 0 8px; border-left: 2px solid black;">Apps</a>
           </div>
-          <div style="text-align: center; margin: 10px auto;">
-      <a href="#" style="margin-right: 10px;"><img src="https://simpleicons.org/icons/x.svg" alt="X" width="40" height="40" style="display: inline-block; vertical-align: middle; border-radius: 50%; border: 2px solid black;"></a>
-      <a href="#" style="margin-right: 10px;"><img src="https://cdn.jsdelivr.net/npm/simple-icons@v5/icons/instagram.svg" alt="Instagram" width="40" height="40" style="display: inline-block; vertical-align: middle; border-radius: 50%; border: 2px solid black;"></a>
-      <a href="#" style="margin-right: 10px;"><img src="https://cdn.jsdelivr.net/npm/simple-icons@v5/icons/facebook.svg" alt="Facebook" width="40" height="40" style="display: inline-block; vertical-align: middle; border-radius: 50%; border: 2px solid black;"></a>
-      <a href="#"><img src="https://cdn.jsdelivr.net/npm/simple-icons@v5/icons/linkedin.svg" alt="LinkedIn" width="40" height="40" style="display: inline-block; vertical-align: middle; border-radius: 50%; border: 2px solid black;"></a>
+          <div style="text-align: center; margin: 20px auto;">
+      <a href="#" style="margin-right: 10px;"><img src="https://michyqruz.github.io/The-money-box/X.png" alt="X" width="35" height="35" style="display: inline-block; vertical-align: middle; border-radius: 50%; border: 2px solid black;"></a>
+      <a href="#" style="margin-right: 10px;"><img src="https://michyqruz.github.io/The-money-box/Instagram.png" alt="Instagram" width="35" height="35" style="display: inline-block; vertical-align: middle; border-radius: 50%; border: 2px solid black;"></a>
+      <a href="#" style="margin-right: 10px;"><img src="https://michyqruz.github.io/The-money-box/Facebook.png" alt="Facebook" width="35" height="35" style="display: inline-block; vertical-align: middle; border-radius: 50%; border: 2px solid black;"></a>
+      <a href="#"><img src="https://michyqruz.github.io/The-money-box/LinkedIn.png" alt="LinkedIn" width="35" height="35" style="display: inline-block; vertical-align: middle; border-radius: 50%; border: 2px solid black;"></a>
           </div>
           <p style="color: #555; font-size: 14px;">
       For more new updates, visit our website! For more new updates, visit our website! For more new updates, visit our website!
